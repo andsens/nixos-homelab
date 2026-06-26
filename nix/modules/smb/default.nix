f98de62b9ffb36882cd902fddc93bd6f0ce35e16
Anders Ingemann <anders@ingemann.de>
2026-06-26 08:38:56 +0200

    smb: Add smbpasswd setup to setup-secrets, use openFirewall option

--- nix/modules/smb/default.nix
+++ nix/modules/smb/default.nix
@@ -1,8 +1,17 @@
 { ... }:
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  users = lib.filterAttrs (user: spec: spec.enable && spec.isNormalUser) config.users.users;
+in
 {
   config = {
     services.samba = {
+      openFirewall = true;
       nmbd.enable = false;
       settings = {
         global = {
@@ -23,17 +32,23 @@
         };
       };
     };
-    services.samba-wsdd.enable = config.services.samba.enable;
-    networking.firewall = lib.mkIf config.services.samba.enable {
-      allowedTCPPorts = [
-        139 # NetBIOS
-        445 # SMB
-        5357 # WSDD
-      ];
-      allowedUDPPorts = [
-        139 # NetBIOS
-        3702 # WSDD
-      ];
+    services.samba-wsdd = {
+      enable = config.services.samba.enable;
+      openFirewall = true;
+    };
+    setup-secrets = {
+      sources = lib.mapAttrs' (
+        user: spec: lib.nameValuePair "SMB_PW_${user}" { description = "SMB Password for ${user}"; }
+      ) users;
+      destinations = lib.mapAttrsToList (user: spec: {
+        logPrefix = "SMB Password for ${user}";
+        requires = [ "SMB_PW_${user}" ];
+        cmd = lib.getExe (
+          pkgs.writeShellScriptBin "set-smbpw.sh" ''
+            printf "%s\n%s\n" "$SMB_PW_${user}" "$SMB_PW_${user}" | smbpasswd -Las "${user}"
+          ''
+        );
+      }) users;
     };
   };
 }
